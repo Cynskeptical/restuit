df4799c73d2edb16b8b41fd4b40fee465a485e8c
Cynskeptical <gonsomn76@gmail.com>
2024-09-05 04:55:37 +0200

    Update restuit_reply.py

--- restuit_reply.py
+++ restuit_reply.py
@@ -1,18 +1,34 @@
+import tweepy
 import os
 
+# Credenciales de Twitter desde los secrets en GitHub
+    consumer_key='tYEkW50YVJh767fIZjcFUy7iD',
+    consumer_secret='3ABDOkjqWjFQCYzvNbZ9FtelSGfwPYS85XKR9gpfFj3HRtEaRC',
+    access_token='1445541467175337994-vH5ctinZJMkB4Nqsv4uTHNfx82DH4N',
+    access_token_secret='jwLL8WrleRoo8IPSlDOqslDW2UJhsj5bEIWpSj7yT1HCi')
+
+# Autenticación con la API de Twitter usando Tweepy
+auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
+api = tweepy.API(auth)
+
 # Aquí procesaremos el tweet para generar la respuesta
-def process_tweet(mention_text):
+def process_tweet(mention_text, user_name):
     response = ""
-    if "brillante" in mention_text:
-        response = "¡Oh, gracias por iluminarme con tu sabiduría brillante! #Ironía"
-    elif "genio" in mention_text:
-        response = "¡Qué humilde eres, me haces sentir tan pequeño! #Sarcasmo"
+    if "brillante" in mention_text.lower():
+        response = f"@{user_name} ¡Oh, gracias por iluminarme con tu sabiduría brillante! #Ironía"
+    elif "genio" in mention_text.lower():
+        response = f"@{user_name} ¡Qué humilde eres, me haces sentir tan pequeño! #Sarcasmo"
     else:
-        response = "¡Gracias por la mención! Ahora cuéntame algo más interesante. #OJO"
+        response = f"@{user_name} ¡Gracias por la mención! Ahora cuéntame algo más interesante. #OJO"
     
     return response
 
-# Simulamos que recibimos una mención de Twitter
-mention_text = "Eres tan brillante"
-response = process_tweet(mention_text)
-print(response)
+# Simulamos que recibimos una mención de Twitter (esto lo recibirás desde IFTTT)
+mention_text = os.getenv('MENTION_TEXT')  # Texto del tweet enviado desde IFTTT
+user_name = os.getenv('MENTION_USER')  # Nombre de usuario del que te mencionó
+
+# Generar la respuesta según la mención
+tweet_response = process_tweet(mention_text, user_name)
+
+# Enviar la respuesta a Twitter
+api.update_status(tweet_response)
